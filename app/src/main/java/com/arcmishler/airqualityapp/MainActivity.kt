a365b69714f5a74dd11b839fd8b69b4b8b35139a
Angela Mishler <arcmishler@gmail.com>
2023-07-23 17:33:42 -0600

    Added dummy bottom navigation bar.

--- app/src/main/java/com/arcmishler/airqualityapp/MainActivity.kt
+++ app/src/main/java/com/arcmishler/airqualityapp/MainActivity.kt
@@ -17,32 +17,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.outlined.North
-import androidx.compose.material.icons.outlined.SwipeUpAlt
-import androidx.compose.material.icons.rounded.SwipeUp
-import androidx.compose.material.icons.rounded.SwipeUpAlt
-import androidx.compose.material.minimumInteractiveComponentSize
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -53,31 +47,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arcmishler.airqualityapp.model.AqiResponse
+import com.arcmishler.airqualityapp.model.GeoCodeResponse
 import com.arcmishler.airqualityapp.model.Pollutant
-import com.arcmishler.airqualityapp.model.PollutantRanges
 import com.arcmishler.airqualityapp.model.PollutantType
 import com.arcmishler.airqualityapp.ui.theme.AirQualityAppTheme
 import com.arcmishler.airqualityapp.viewmodel.AirQualityViewModel
@@ -97,10 +82,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AirQualityApp(viewModel: AirQualityViewModel = viewModel()) {
-    val data = viewModel.pollutantList.collectAsState()
     val (searchText, setSearchText) = remember { mutableStateOf("") }
+    val pollutants by viewModel.pollutantList.collectAsState()
+    val airQuality by viewModel.aqiData.collectAsState()
+    val gcData by viewModel.geoCodeData.collectAsState()
 
-    AirScreen(searchText, setSearchText, viewModel)
+    AirScreen(searchText, setSearchText, pollutants, airQuality, gcData, viewModel)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -109,82 +96,110 @@ fun AirQualityApp(viewModel: AirQualityViewModel = viewModel()) {
 fun AirScreen(
     searchText: String,
     onSearchTextChange: (String) -> Unit,
+    pollutants: List<Pollutant>?,
+    airQuality: AqiResponse?,
+    gcData: GeoCodeResponse?,
     viewModel: AirQualityViewModel
 ) {
-    val aqData = viewModel.pollutantList.collectAsState()
-    val gcData = viewModel.geoCodeData.collectAsState()
-    val pollutants by viewModel.pollutantList.collectAsState()
-    val airQuality by viewModel.aqiData.collectAsState()
     var active by remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        SearchBar(
-            modifier = Modifier.fillMaxWidth(),
-            query = searchText,
-            onQueryChange = { onSearchTextChange(it) },
-            onSearch = {
-                active = false
-                if (searchText.isNotEmpty()) {
-                    // TODO: Input validation
-                    viewModel.fetchGeoCode(searchText)
-                }
-            },
-            active = active, onActiveChange = { active = it },
-            placeholder = {
-                Text("Enter zipcode")
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
-            },
-            trailingIcon = {
-                IconButton(
-                    onClick = {
-                        if (active) {
-                            active = false
-                        }
-                    },
-                ) {
-                    Icon(Icons.Default.Close, "Close button")
-                }
-            }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        }
-        if (pollutants == null) {
-            EmptyAirQuality()
-        } else {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Box(modifier = Modifier
-                    .height(160.dp)
-                    .offset(y = 10.dp)
-                    .fillMaxSize(),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    AQIColorChart(aqi = airQuality?.overallAqi)
-                    AQIGauge(aqi = airQuality?.overallAqi)
-                    AQIText(aqi = airQuality?.overallAqi.toString())
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-                LocationHeader(gcData.value?.name)
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    contentPadding = PaddingValues(top = 10.dp, start = 0.dp, end = 0.dp, bottom = 24.dp),
-                    horizontalArrangement = Arrangement.spacedBy((-50).dp),
-                    verticalArrangement = Arrangement.spacedBy((-5).dp)
-                ) {
-                    items(pollutants!!) { pollutant ->
-                        AirQualityCard(pollutant)
+            SearchBar(
+                modifier = Modifier.fillMaxWidth(),
+                query = searchText,
+                onQueryChange = { onSearchTextChange(it) },
+                onSearch = {
+                    active = false
+                    if (searchText.isNotEmpty()) {
+                        // TODO: Input validation
+                        viewModel.fetchGeoCode(searchText)
+                    }
+                },
+                active = active, onActiveChange = { active = it },
+                placeholder = {
+                    Text("Enter zipcode")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+                },
+                trailingIcon = {
+                    IconButton(
+                        onClick = {
+                            if (active) {
+                                active = false
+                            }
+                        },
+                    ) {
+                        Icon(Icons.Default.Close, "Close button")
                     }
                 }
+            ) {
+            }
+            if (pollutants == null) {
+                EmptyAirQuality()
+            } else {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    AQIDisplay(airQuality)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    LocationHeader(gcData?.name)
+                    PollutantGrid(pollutants)
+                }
+            }
+        }
+
+        // Place the Bottom Navigation Bar at the bottom of the screen
+        var selectedItem by remember { mutableStateOf(0) }
+        val screens = listOf("Current", "Historic", "Trends")
+        NavigationBar(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            screens.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
             }
         }
     }
 }
 
+@Composable
+fun AQIDisplay(airQuality: AqiResponse?) {
+    Box(modifier = Modifier
+        .height(160.dp)
+        .offset(y = 10.dp)
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        AQIColorChart(aqi = airQuality?.overallAqi)
+        AQIGauge(aqi = airQuality?.overallAqi)
+        AQIText(aqi = airQuality?.overallAqi.toString())
+    }
+}
+
+@Composable
+fun PollutantGrid(pollutants: List<Pollutant>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(top = 10.dp, start = 0.dp, end = 0.dp, bottom = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy((-50).dp),
+        verticalArrangement = Arrangement.spacedBy((-5).dp)
+    ) {
+        items(pollutants) { pollutant ->
+            AirQualityCard(pollutant)
+        }
+    }
+}
 @Composable
 fun LocationHeader(location: String?) {
     Card(modifier = Modifier,
@@ -227,7 +242,7 @@ fun AQIText(aqi: String) {
         verticalArrangement = Arrangement.spacedBy((-8).dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("AQI", style = MaterialTheme.typography.bodyLarge)
+        Text("AQI", fontSize = 22.sp)
         Text(aqi, style = MaterialTheme.typography.headlineLarge, fontWeight = FontWeight.Bold)
     }
 }
@@ -285,11 +300,11 @@ fun AQIGauge(aqi: Int?) {
     })
 }
 
-@Preview
-@Composable
-fun LocationHeaderPreview() {
-    LocationHeader(location = "Salt Lake City")
-}
+//@Preview
+//@Composable
+//fun LocationHeaderPreview() {
+//    LocationHeader(location = "Salt Lake City")
+//}
 //
 //@Preview
 //@Composable
@@ -332,16 +347,15 @@ fun AirQualityCard(pollutant: Pollutant) {
         elevation = CardDefaults.cardElevation(10.dp)) {
         Column(
             modifier = Modifier
-                .padding(5.dp)
+                .padding(1.dp)
                 .requiredHeight(110.dp)
-                .requiredWidth(135.dp),
+                .requiredWidth(150.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.aligned(Alignment.CenterVertically)
         ) {
             when (pollutant.type) {
                 PollutantType.PM2_5 -> {
-                    Text(style = MaterialTheme.typography.headlineLarge,
-                        fontWeight = FontWeight.Bold,
+                    Text(fontSize = 22.sp,
                         color = Color.White,
                         text = buildAnnotatedString {
                             append("PM")
@@ -351,8 +365,7 @@ fun AirQualityCard(pollutant: Pollutant) {
                         })
                 }
                 PollutantType.PM10 -> {
-                    Text(style = MaterialTheme.typography.headlineLarge,
-                        fontWeight = FontWeight.Bold,
+                    Text(fontSize = 22.sp,
                         color = Color.White,
                         text = buildAnnotatedString {
                             append("PM")
@@ -364,16 +377,16 @@ fun AirQualityCard(pollutant: Pollutant) {
                 else -> {
                     Text(
                         pollutant.type.toString(),
-                        style = MaterialTheme.typography.headlineLarge,
-                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp,
                         color = Color.White
                     )
                 }
             }
-            Spacer(Modifier.height(4.dp))
+            Spacer(Modifier.height(0.dp))
             Text(
                 "${pollutant.value}",
-                fontSize = 24.sp,
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
                 color = Color.White)
         }
     }
